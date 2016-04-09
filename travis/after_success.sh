9a6cde8e8b78ffd742cf3cb09914515420aa7db4
Eduardo Macarron <eduardo.macarron@gmail.com>
2016-04-09 07:46:05 +0200

    fix travis post-script

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -26,7 +26,7 @@ echo "Java detected: ${VER}"
 # 2. Deploy site
 # 3. Use -q option to only display Maven errors and warnings.
 
-if [ "$mybatis_repo" == "https://github.com/mybatis/hazelcast-cache.git" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$mybatis_repo" == "https://github.com/mybatis/caffeine-cache.git" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   if [ $VER == "16" ]; then
     mvn clean deploy -q --settings ./travis/settings.xml
     echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
