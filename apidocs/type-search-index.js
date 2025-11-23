c79b082859f15f307989d16d7a1fe5acd8faf8ec
hazendaz <jeremylandis@hotmail.com>
2025-11-23 16:27:11 +0000

    Deploying to gh-pages from @ mybatis/caffeine-cache@7d451e65a1a3ec752bf076ff76424a8ee80b4f23 🚀

--- apidocs/type-search-index.js
+++ apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.mybatis.caches.caffeine","l":"CaffeineCache"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.mybatis.caches.caffeine","l":"CaffeineCache"}];updateSearchResults();
